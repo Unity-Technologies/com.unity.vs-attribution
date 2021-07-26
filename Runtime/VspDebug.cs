ff817f1f0efad60f0e3b9dbe0be47a2bee6e296f
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2021-07-26 09:59:30 +0300

    Added VspDebug.cs with conditional compilation

---
+++ Runtime/VspDebug.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor.VSP
+{
+    internal static class VspDebug
+    {
+        private static readonly string k_VspPrefix = "[VSP Analytics] ";
+
+        public static void Log(string message)
+        {
+#if VSP_ANALYTICS_DEBUG
+            string finalMessage = k_VspPrefix + message;
+            Debug.Log(message);
+#endif
+        }
+        
+        public static void LogWarning(string message)
+        {
+#if VSP_ANALYTICS_DEBUG
+            string finalMessage = k_VspPrefix + message;
+            Debug.LogWarning(message);
+#endif
+        }
+        
+        public static void LogError(string message)
+        {
+#if VSP_ANALYTICS_DEBUG
+            string finalMessage = k_VspPrefix + message;
+            Debug.LogError(message);
+#endif
+        }
+    }
+}
