3bd67982d5793bdda114d7fe103ee025a46a5fd9
Aivaras Ivoškus <aiva.ivoskus@gmail.com>
2022-02-14 10:17:39 +0200

    Enabling Analytics tracking from the Setup method

--- Tests/Runtime/VspAttributionTests.cs
+++ Tests/Runtime/VspAttributionTests.cs
@@ -5,11 +5,19 @@ namespace UnityEngine.VspAttribution.Tests
 {
     public class VspAttributionTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            Analytics.Analytics.enabled = true;
+            Analytics.Analytics.limitUserTracking = false;
+            Analytics.Analytics.ResumeInitialization();
+        }
+        
         [Test]
         public void SendAttributionEvent_Returns_Ok()
         {
             AnalyticsResult result = VspAttribution.SendAttributionEvent("testAction", "testPartner", "testCustomerUid");
-            Assert.AreEqual(result, AnalyticsResult.Ok);
+            Assert.AreEqual(AnalyticsResult.Ok, result);
         }
     }
 }
